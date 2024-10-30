f6eb23c81c692b28335254df5f601a5043e70a85
Fezao187 <katlegomaredi9@gmail.com>
2024-10-30 16:06:32 +0200

    Controller, Repository and Service annotation

--- tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/SpringAnnotationsApplication.java
+++ tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/SpringAnnotationsApplication.java
@@ -1,6 +1,9 @@
 package net.javaguidelines.spring_annotations;
 
+import net.javaguidelines.spring_annotations.controllers.MyController;
 import net.javaguidelines.spring_annotations.controllers.PizzaController;
+import net.javaguidelines.spring_annotations.repository.MyRepository;
+import net.javaguidelines.spring_annotations.service.MyService;
 import net.javaguidelines.spring_annotations.service.VegPizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,16 @@ public class SpringAnnotationsApplication {
 
 	public static void main(String[] args) {
 		var context = SpringApplication.run(SpringAnnotationsApplication.class, args);
-		PizzaController pizzaController= context.getBean(PizzaController.class);
-		System.out.println(pizzaController.getPizza());
+//		PizzaController pizzaController= context.getBean(PizzaController.class);
+//		System.out.println(pizzaController.getPizza());
+MyController myController=context.getBean(MyController.class);
+		System.out.println(myController.hello());
 
+		MyService myService=context.getBean(MyService.class);
+		System.out.println(myService.hello());
+
+		MyRepository myRepository=context.getBean(MyRepository.class);
+		System.out.println(myRepository.hello());
 //		VegPizza vegPizza= context.getBean(VegPizza.class);
 //		System.out.println(vegPizza.getPizza());
 	}
