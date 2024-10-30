a20e7d1464482822a84ae1fa7f1edbb05a7a141e
Fezao187 <katlegomaredi9@gmail.com>
2024-10-30 19:55:53 +0200

    PostMapping and ResponseStatus annotation

--- tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/controllers/BookController.java
+++ tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/controllers/BookController.java
@@ -1,7 +1,9 @@
 package net.javaguidelines.spring_annotations.controllers;
 
 import net.javaguidelines.spring_annotations.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,19 +19,29 @@ public class BookController {
         return "Hello World";
     }
 
-//    @RequestMapping(value = {"/book", "/core-java", "/java"},
+    //    @RequestMapping(value = {"/book", "/core-java", "/java"},
 //            method = RequestMethod.GET,
 //            produces = MediaType.APPLICATION_JSON_VALUE
 ////            consumes = MediaType.APPLICATION_JSON_VALUE
 //    )
-@GetMapping(value = {"/book", "/core-java", "/java"},
-        produces = MediaType.APPLICATION_JSON_VALUE
-)
+    @GetMapping(value = {"/book", "/core-java", "/java"},
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
 //    @ResponseBody
     public Book getBook() {
         Book book = new Book(1, "Core Java", "Learn core Java and features");
         return book;
     }
 
-    public
+//    @RequestMapping(value = "/books/create",
+//            method = RequestMethod.POST)
+    @PostMapping(value = "/books/create",
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
 }
