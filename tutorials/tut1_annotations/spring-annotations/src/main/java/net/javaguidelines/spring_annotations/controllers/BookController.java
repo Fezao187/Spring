52d0be70472c7a53a9695660834a866ca6db39f3
Fezao187 <katlegomaredi9@gmail.com>
2024-10-30 19:26:34 +0200

    RequestMapping annotation

--- tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/controllers/BookController.java
+++ tutorials/tut1_annotations/spring-annotations/src/main/java/net/javaguidelines/spring_annotations/controllers/BookController.java
@@ -1,14 +1,17 @@
 package net.javaguidelines.spring_annotations.controllers;
 
 import net.javaguidelines.spring_annotations.beans.Book;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller // For spring MVC apps
 //@ResponseBody
 @RestController // for rest apis
+@RequestMapping("/api")
 public class BookController {
 
     @RequestMapping("/hello-world")
@@ -17,10 +20,14 @@ public class BookController {
         return "Hello World";
     }
 
-    @RequestMapping("/book")
+    @RequestMapping(value = {"/book", "/core-java", "/java"},
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+//            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
 //    @ResponseBody
     public Book getBook() {
-        Book book = new Book(1,"Core Java", "Learn core Java and features");
+        Book book = new Book(1, "Core Java", "Learn core Java and features");
         return book;
     }
 }
